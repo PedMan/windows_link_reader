beb8725cff6346e332e62761e3fd83a68b162594
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-09-23 22:09:26 +0200

    Update lnkReader.c

--- scripts/lnkReader.c
+++ scripts/lnkReader.c
@@ -1,67 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <wchar.h>
-#include <wctype.h>
-#include <locale.h>
+#include <regex.h>
 
 #define MAX_DATA_SIZE 4096
 
-char* bytesToHexString(const unsigned char* bytes, int length) {
-    char* hexStr = malloc(2 * length + 1);
-    for (int i = 0; i < length; ++i) {
-        sprintf(hexStr + 2 * i, "%02X", bytes[i]);
-    }
-    hexStr[2 * length] = '\0';
-    return hexStr;
-}
-
-char* hexToASCII(const char* hexStr) {
-    int length = strlen(hexStr) / 2;
+char* binaryToASCII(const unsigned char* data, int length) {
     char* asciiStr = malloc(length + 1);
     for (int i = 0; i < length; i++) {
-        sscanf(hexStr + 2 * i, "%2hhx", &asciiStr[i]);
+        if ((data[i] >= 32 && data[i] <= 126) || data[i] == '\n' || data[i] == '\t') {
+            asciiStr[i] = data[i];
+        } else {
+            asciiStr[i] = ' ';
+        }
     }
     asciiStr[length] = '\0';
     return asciiStr;
 }
 
-char* findPathInHexString(const char* hexStr) {
-    const char* potentialDrives = "4142434445464748494A4B4C4D4E4F505152535455565758595A"; // ABCDEFGHIJKLMNOPQRSTUVWXYZ in hex
-    for (int i = 0; i < strlen(potentialDrives); i += 2) {
-        char pattern[5];
-        strncpy(pattern, potentialDrives + i, 2);
-        pattern[2] = '3'; // This is for ":"
-        pattern[3] = 'A'; // This is for "\"
-        pattern[4] = '\0';
+char* findLongestValidPath(const char* str) {
+    regex_t regex;
+    regmatch_t matches[2];
+    char pattern[] = "([A-Za-z]:[\\\\/][^ ]+?[\\\\/][^ ]+)";
+    char* longestPath = NULL;
 
-        char* startOfPath = strstr(hexStr, pattern);
-        if (startOfPath) {
-            char* endOfPath = strstr(startOfPath, "2000"); // This is for space
-            if (endOfPath) {
-                int pathLength = endOfPath - startOfPath;
-                char* path = malloc(pathLength + 1);
-                strncpy(path, startOfPath, pathLength);
-                path[pathLength] = '\0';
-                char* asciiPath = hexToASCII(path);
-                free(path);
-                return asciiPath;
-            }
-        }
+    if (regcomp(&regex, pattern, REG_EXTENDED)) {
+        system("zenity --error --text='Failed to compile regex.'");
+        return NULL;
     }
-    return NULL;
+
+    const char* currentSearch = str;
+    while (!regexec(&regex, currentSearch, 2, matches, 0)) {
+        int start = matches[1].rm_so;
+        int end = matches[1].rm_eo;
+        
+        if(!longestPath || (end - start) > strlen(longestPath)) {
+            free(longestPath);
+            longestPath = malloc(end - start + 1);
+            strncpy(longestPath, currentSearch + start, end - start);
+            longestPath[end - start] = '\0';
+        }
+        currentSearch += end;
+    }
+
+    regfree(&regex);
+    return longestPath;
 }
 
 int main(int argc, char* argv[]) {
-    setlocale(LC_ALL, "");
     if (argc != 2) {
-        printf("Usage: %s <path_to_lnk_file>\n", argv[0]);
+        system("zenity --error --text='Incorrect number of arguments.'");
         return 1;
     }
 
     FILE* file = fopen(argv[1], "rb");
     if (!file) {
-        perror("Erreur lors de l'ouverture du fichier");
+        system("zenity --error --text='Error opening the .lnk file.'");
         return 1;
     }
 
@@ -69,26 +63,29 @@ int main(int argc, char* argv[]) {
     int bytesRead = fread(data, 1, MAX_DATA_SIZE, file);
     fclose(file);
 
-    char* hexStr = bytesToHexString(data, bytesRead);
-    char* foundPath = findPathInHexString(hexStr);
-    if (foundPath) {
-        printf("Chemin trouvé: %s\n", foundPath);   
+    char* asciiData = binaryToASCII(data, bytesRead);
+    char* foundPath = findLongestValidPath(asciiData);
 
-        // Remplacer les backslashes par des slashes pour la compatibilité avec Linux.
+    if (foundPath) {
         for (int i = 0; foundPath[i]; i++) {
             if (foundPath[i] == '\\') {
                 foundPath[i] = '/';
             }
-        }   
+        }
 
         char cmd[1024];
-        sprintf(cmd, "xdg-open \"%s\"", foundPath);
-        system(cmd);
+        sprintf(cmd, "xdg-open '%s'", foundPath);
+        if (system(cmd) != 0) {
+            char errMsg[512];
+            sprintf(errMsg, "zenity --error --text='Error opening path: %s'", foundPath);
+            system(errMsg);
+        }
+
         free(foundPath);
     } else {
-        printf("Chemin non trouvé.\n");
-    }   
+        system("zenity --error --text='Path not found in the provided .lnk file.'");
+    }
 
-    free(hexStr);
+    free(asciiData);
     return 0;
 }
