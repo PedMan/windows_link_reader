9bc5897eec00d6d4ece2b123b4a2a42857be03f0
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-09-23 20:32:28 +0200

    Update lnkReader.c

--- scripts/lnkReader.c
+++ scripts/lnkReader.c
@@ -1,106 +1,94 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <wchar.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
+#include <wctype.h>
+#include <locale.h>
 
-#pragma pack(push, 1)
+#define MAX_DATA_SIZE 4096
 
-typedef struct {
-    uint32_t headerSize;
-    uint32_t linkCLSID[4];
-    uint32_t flags;
-    uint32_t fileAttributes;
-    uint64_t creationTime;
-    uint64_t accessTime;
-    uint64_t writeTime;
-    uint32_t fileSize;
-    uint32_t iconIndex;
-    uint32_t showCommand;
-    uint16_t hotKey;
-    uint16_t reserved1;
-    uint32_t reserved2;
-    uint32_t reserved3;
-} LINK_HEADER;
-
-typedef struct {
-    uint32_t length;
-    uint16_t firstOffset;
-    uint32_t size;
-    uint32_t flags;
-    uint32_t offsetLocalVolumeTable;
-    uint32_t offsetLocalBasePath;
-    uint32_t offsetNetworkVolumeTable;
-    uint32_t offsetCommonNetworkRelativeLink;
-    uint32_t offsetCommonPathSuffix;
-} FILE_LOCATION_INFO;
-
-#pragma pack(pop)
-
-void readLinkTarget(FILE *file, const LINK_HEADER *header) {
-    if (header->flags & 0x1) { // HasLinkTargetIDList
-        uint16_t idListSize;
-        fread(&idListSize, sizeof(idListSize), 1, file);
-        fseek(file, idListSize, SEEK_CUR); // Skip the IDList
+char* bytesToHexString(const unsigned char* bytes, int length) {
+    char* hexStr = malloc(2 * length + 1);
+    for (int i = 0; i < length; ++i) {
+        sprintf(hexStr + 2 * i, "%02X", bytes[i]);
     }
-
-    if (header->flags & 0x2) { // HasLinkInfo
-        uint32_t linkInfoSize;
-        fread(&linkInfoSize, sizeof(linkInfoSize), 1, file);
-        fseek(file, -sizeof(linkInfoSize), SEEK_CUR); // Reposition to read LINKINFO
-
-        char *linkInfo = malloc(linkInfoSize);
-        fread(linkInfo, linkInfoSize, 1, file);
-
-        // Decode the LinkInfo structure to extract path
-        uint32_t linkInfoHeaderSize;
-        memcpy(&linkInfoHeaderSize, linkInfo, sizeof(uint32_t));
-
-        if (linkInfoHeaderSize >= 0x24) { // Minimum size for LinkInfo
-            uint32_t localBasePathOffset;
-            memcpy(&localBasePathOffset, linkInfo + 0x10, sizeof(uint32_t));
-
-            if (localBasePathOffset > 0) {
-                wchar_t *localBasePath = (wchar_t *)(linkInfo + localBasePathOffset);
-                wprintf(L"Local Base Path: %ls\n", localBasePath);
-            }
-        }
-
-        free(linkInfo);
-    }
+    hexStr[2 * length] = '\0';
+    return hexStr;
 }
 
+char* hexToASCII(const char* hexStr) {
+    int length = strlen(hexStr) / 2;
+    char* asciiStr = malloc(length + 1);
+    for (int i = 0; i < length; i++) {
+        sscanf(hexStr + 2 * i, "%2hhx", &asciiStr[i]);
+    }
+    asciiStr[length] = '\0';
+    return asciiStr;
+}
 
-int main(int argc, char *argv[]) {
+char* findPathInHexString(const char* hexStr) {
+    const char* potentialDrives = "4142434445464748494A4B4C4D4E4F505152535455565758595A"; // ABCDEFGHIJKLMNOPQRSTUVWXYZ in hex
+    for (int i = 0; i < strlen(potentialDrives); i += 2) {
+        char pattern[5];
+        strncpy(pattern, potentialDrives + i, 2);
+        pattern[2] = '3'; // This is for ":"
+        pattern[3] = 'A'; // This is for "\"
+        pattern[4] = '\0';
+
+        char* startOfPath = strstr(hexStr, pattern);
+        if (startOfPath) {
+            char* endOfPath = strstr(startOfPath, "2000"); // This is for space
+            if (endOfPath) {
+                int pathLength = endOfPath - startOfPath;
+                char* path = malloc(pathLength + 1);
+                strncpy(path, startOfPath, pathLength);
+                path[pathLength] = '\0';
+                char* asciiPath = hexToASCII(path);
+                free(path);
+                return asciiPath;
+            }
+        }
+    }
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "");
     if (argc != 2) {
         printf("Usage: %s <path_to_lnk_file>\n", argv[0]);
         return 1;
     }
 
-    FILE *file = fopen(argv[1], "rb");
+    FILE* file = fopen(argv[1], "rb");
     if (!file) {
         perror("Erreur lors de l'ouverture du fichier");
         return 1;
     }
 
-    LINK_HEADER header;
-    fread(&header, sizeof(LINK_HEADER), 1, file);
-    
-    // Debugging outputs
-    printf("LINK_HEADER headerSize: %u\n", header.headerSize);
-    printf("LINK_HEADER flags: %u\n", header.flags);
-
-
-    FILE_LOCATION_INFO locInfo;
-    fread(&locInfo, sizeof(FILE_LOCATION_INFO), 1, file);
-    printf("FILE_LOCATION_INFO length: %u\n", locInfo.length);
-    printf("FILE_LOCATION_INFO firstOffset: %u\n", locInfo.firstOffset);
-    printf("FILE_LOCATION_INFO size: %u\n", locInfo.size);
-    printf("FILE_LOCATION_INFO flags: %u\n", locInfo.flags);    
-    
-
-    readLinkTarget(file, &header);
-
+    unsigned char data[MAX_DATA_SIZE];
+    int bytesRead = fread(data, 1, MAX_DATA_SIZE, file);
     fclose(file);
+
+    char* hexStr = bytesToHexString(data, bytesRead);
+    char* foundPath = findPathInHexString(hexStr);
+    if (foundPath) {
+        printf("Chemin trouvé: %s\n", foundPath);   
+
+        // Remplacer les backslashes par des slashes pour la compatibilité avec Linux.
+        for (int i = 0; foundPath[i]; i++) {
+            if (foundPath[i] == '\\') {
+                foundPath[i] = '/';
+            }
+        }   
+
+        char cmd[1024];
+        sprintf(cmd, "xdg-open \"%s\"", foundPath);
+        system(cmd);
+        free(foundPath);
+    } else {
+        printf("Chemin non trouvé.\n");
+    }   
+
+    free(hexStr);
     return 0;
 }
