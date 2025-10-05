94fff46ffd6810cac35eb556032b9dabe362da38
PedMan <PedMan@users.noreply.github.com>
2025-10-05 02:39:31 -0400

    Update uninstall.sh
    Added removal for config file.

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 echo "[*] Removing open_lnk binary..."
 sudo rm -f /usr/local/bin/open_lnk
 
+echo "[*] Removing config file..."
+sudo rm -f /etc/lnkReader.conf
+
 echo "[*] Removing .desktop entry..."
 sudo rm -f /usr/share/applications/open_lnk.desktop
 
